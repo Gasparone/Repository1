9504e98c01d4d0ebd2df99ca42ee02fc8e48df2a
Alunno <Alunno@PC-20-INF4>
2017-10-16 12:15:15 +0200

    Super Progetto

---
+++ ThreadMultiServer/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+			try {
+				MultiServer frame = new MultiServer();
+				frame.setVisible(true);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
+
+}
